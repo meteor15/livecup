e373920b2fbe71396b0b8539d28f2547d3b866cd
meteor15 <magoz@mail.ru>
2015-09-03 01:09:06 +0500

    Функционал для переводов через CPhpMessageSource

--- protected/config/main.php
+++ protected/config/main.php
@@ -23,15 +23,20 @@ return array(
 		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'pass',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
 		*/
 	),
 
+    'language'=>'ru',
+
 	// application components
 	'components'=>array(
+        'messages'=>array(
+            'class'=>'CPhpMessageSource',
+        ),
 
 		'user'=>array(
 			// enable cookie-based authentication
